86858a19e4aba941425a6219fbe4449f5b5baf54
javier-camilo <camiloviloriatovar@gmail.com>
2020-11-09 23:52:22 -0500

    first commit

---
+++ PulsacionesApi/Models/Persona.cs
@@ -0,0 +1,14 @@
+namespace PulsacionesApi.Models
+{
+    public class Persona
+    {
+
+        public string Nombre { get; set; }
+        public string Identificacion { get; set; }
+        public int Edad { get; set; }
+        public string Sexo { get; set; }
+
+        public 
+        
+    }
+}

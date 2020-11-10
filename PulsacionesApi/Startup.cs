af7476cc53b9cd5a14631c4c32210d219e5c19a5
javier-camilo <camiloviloriatovar@gmail.com>
2020-11-10 12:03:15 -0500

    cambios en la api web

--- PulsacionesApi/Startup.cs
+++ PulsacionesApi/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.Logging;
 
 using Microsoft.EntityFrameworkCore;
 using PulsacionesApi.Context;
+using Microsoft.OpenApi.Models;
 
 namespace PulsacionesApi
 {
@@ -28,6 +29,30 @@ namespace PulsacionesApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "School API",
+                    Description = "School API - ASP.NET Core Web API",
+                    TermsOfService = new Uri("https://cla.dotnetfoundation.org/"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Unicesar",
+                        Email = string.Empty,
+                        Url = new Uri("https://github.com/borisgr04/CrudNgDotNetCore3"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Licencia dotnet foundation",
+                        Url = new Uri("https://www.byasystems.co/license"),
+                    }
+                });
+            });
+
             
             var connectionString=Configuration.GetConnectionString("DefaultConnection");
             services.AddDbContext<PulsacionesContext>(p=>p.UseSqlServer(connectionString));
@@ -53,6 +78,13 @@ namespace PulsacionesApi
             {
                 endpoints.MapControllers();
             });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
         }
     }
 }
